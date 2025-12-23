ea7a423b10947cd17771bb769aaf671b0e51edea
Inaya-TheCoder <inaya.singh2013@gmail.com>
2025-12-23 21:27:34 +0530

    date and time

---
+++ date and time.py
@@ -0,0 +1,6 @@
+from datetime import date , time , datetime
+today= date.today()
+now = datetime.now()
+print ("Today's Date is",today)
+print ("The Time right now is",now)
+print(today.year, today.month , today.day)
