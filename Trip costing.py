b96f76ca8dd8b57094f162ecf20cf2477e9cab71
Inaya-TheCoder <inaya.singh2013@gmail.com>
2026-01-13 17:59:19 +0530

    homework

---
+++ Trip costing.py
@@ -0,0 +1,5 @@
+def hotel_cost(nights):
+    return 150*nights
+def plane_cost(city):
+    if "mumbai" == city
+    
---
+++ homework.py
@@ -0,0 +1,6 @@
+import calendar
+year = 2026
+month = 1
+for i in range(1,12):
+    a = i + 1
+    print (calendar.month(year,a))
